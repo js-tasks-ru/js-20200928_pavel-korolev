106abfd2ce231d3b5b188cac89f4f43ea2d365bb
Pavel Korolev <p.korolev@.lamoda.ru>
2020-10-07 17:28:43 +0300

    03-objects-arrays-intro-to-testing solved

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,15 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  return string.split('').reduce((acc, char) => {
+    return acc.split(char).length > size ? acc : `${acc}${char}`;
+  }, '');
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,26 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
 
+/**
+ * "читерский" вариант через Set =)
+ *
+ * export function uniq(arr = []) {
+ *  return [...new Set(arr)];
+ * }
+*/
+
+export function uniq(arr = []) {
+  return Object
+    .values(
+      arr.reduce((cache, value, index) => {
+        if (!cache.hasOwnProperty(value)) {
+          cache[value] = { index, value };
+        }
+
+        return cache;
+      }, {})
+    )
+    .sort((a, b) => a.index - b.index)
+    .map(({ value }) => value);
 }
