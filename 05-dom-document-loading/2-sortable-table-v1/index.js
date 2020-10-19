be619f7a67cfe7fb46a4ca5b2363f23435bd3217
Pavel Korolev <p.korolev@.lamoda.ru>
2020-10-19 23:41:58 +0300

    05-dom-document-loading, 06-events-practice solved

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,155 @@
 export default class SortableTable {
+  headerRefs = {};
+  subElements = {};
+
+  constructor(headerOptions, { data } = {}) {
+    this.headerData = headerOptions;
+    this.bodyData = data;
+    this.template = `
+      <div class="sortable-table">
+        <div data-element="header" class="sortable-table__header sortable-table__row"></div>
+        <div data-element="body" class="sortable-table__body"></div>
+        <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+        <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+          <div>
+            <p>No products satisfies your filter criteria</p>
+            <button type="button" class="button-primary-outline">Reset all filters</button>
+          </div>
+        </div>
+      <div>
+    `;
+    this.render();
+  }
+
+  createRefs(html) {
+    [...html.querySelectorAll('[data-element]')].forEach(el => {
+      this.subElements[el.dataset.element] = el;
+    });
+  }
+
+  createHeaderRefs(html) {
+    [...html.querySelectorAll('[data-id]')].forEach(el => {
+      this.headerRefs[el.dataset.id] = el;
+    });
+    this.headerRefs.arrow = this.renderArrow();
+  }
+
+  renderArrow() {
+    const el = document.createElement('div');
+    el.innerHTML = `
+      <span data-element="arrow" class="sortable-table__sort-arrow">
+        <span class="sort-arrow"></span>
+      </span>
+    `;
+    return el.firstElementChild;
+  }
+
+  render() {
+    const el = document.createElement('div');
+    el.innerHTML = this.template;
+    this.createRefs(el);
+    this.subElements.header.innerHTML = this.getHeaderTemplate();
+    this.createHeaderRefs(this.subElements.header);
+    this.subElements.body.innerHTML = this.getBodyTemplate(this.bodyData);
+    this.element = el.firstElementChild;
+  }
+
+  getHeaderTemplate() {
+    return this.headerData.reduce((html, { id, title, sortable }) => {
+      return `
+        ${html}
+        <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="">
+          <span>${title}</span>
+        </div>
+      `;
+    }, '');
+  }
+
+  getBodyTemplate(bodyData) {
+    return bodyData.reduce((html, { id, title, images, quantity, price, sales }) => {
+      return `
+        ${html}
+        <a href="/products/${id}" class="sortable-table__row">
+          ${this.getRowImageTemplate(images)}
+          ${this.getRowCellsTemplate([title, quantity, price, sales])}
+        </a>
+      `;
+    }, '');
+  }
+
+  getRowImageTemplate(images) {
+    if (!images) {
+      return '';
+    }
+
+    const [firstImage] = images;
+
+    return `
+      <div class="sortable-table__cell">
+        <img class="sortable-table-image" alt="Image" src="${firstImage.url}">
+      </div>
+    `;
+  }
+
+  getRowCellsTemplate(cells) {
+    return cells
+      .filter(val => val !== undefined)
+      .reduce((acc, val) => `${acc}<div class="sortable-table__cell">${val}</div>`, '');
+  }
+
+  sort(field, sortOrder) {
+    const { sortType } = this.headerData.find(obj => obj.id === field);
+    this.headerRefs[field].dataset.order = sortOrder;
+    this.headerRefs[field].append(this.headerRefs.arrow);
+    this.subElements.body.innerHTML = this.getBodyTemplate(
+      this.getSortedBodyData(field, sortOrder, sortType)
+    );
+  }
+
+  getSortedBodyData(field, sortOrder, sortType) {
+    switch (sortType) {
+      case 'string':
+        return this.sortByStrings(this.bodyData, field, sortOrder);
+      case 'number':
+        return this.sortByNumbers(this.bodyData, field, sortOrder);
+    }
+  }
+
+  sortByNumbers(arr, key, sortOrder) {
+    const compare = (a, b) => {
+      switch (sortOrder) {
+        case 'asc':
+          return a[key] - b[key];
+        case 'desc':
+          return b[key] - a[key];
+        // no default
+      }
+    };
+
+    return arr.slice().sort(compare);
+  }
+
+  sortByStrings(arr, key, sortOrder) {
+    const collator = new Intl.Collator(['ru', 'en'], { caseFirst: 'upper' });
+    const compare = (a, b) => {
+      switch (sortOrder) {
+        case 'asc':
+          return collator.compare(a[key], b[key]);
+        case 'desc':
+          return collator.compare(b[key], a[key]);
+        // no default
+      }
+    };
+
+    return arr.slice().sort(compare);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
-
--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,190 @@
 export default class DoubleSlider {
+  refs = {};
+  currentThumb = null;
 
+  constructor({
+    min = 0,
+    max = 0,
+    formatValue = (value) => value,
+    selected,
+  } = {}) {
+    this.min = min;
+    this.max = max;
+    this.range = max - min;
+    this.selected = selected;
+    this.formatValue = formatValue;
+
+    this.position = {
+      thumbL: { shiftX: 0, sliderLeft: 0, last: 0 },
+      thumbR: { shiftX: 0, sliderRight: 0, last: 0 },
+    };
+
+    this.selection = {
+      from: selected && selected.from || min,
+      to: selected && selected.to || max,
+    };
+
+    this.render();
+    this.initEventListeners();
+  }
+
+  get template() {
+    let min = this.min;
+    let max = this.max;
+    let left = 0;
+    let right = 0;
+
+    if (this.selected) {
+      min = this.selected.from;
+      max = this.selected.to;
+      left = (min - this.min) / this.range * 100;
+      right = (max - this.min) / this.range * 100;
+    }
+
+    return `
+      <div class="range-slider">
+        <span data-element="from">${this.formatValue(min)}</span>
+        <div class="range-slider__inner" data-element="slider">
+          <span class="range-slider__progress" data-element="progress" style="left: ${left}%; right: ${right}%"></span>
+          <span class="range-slider__thumb-left" data-element="thumbL" style="left: ${left}%"></span>
+          <span class="range-slider__thumb-right" data-element="thumbR" style="right: ${right}%"></span>
+        </div>
+        <span data-element="to">${this.formatValue(max)}</span>
+      </div>
+    `;
+  }
+
+  createRefs(html) {
+    [...html.querySelectorAll('[data-element]')].forEach(el => {
+      this.refs[el.dataset.element] = el;
+    });
+  }
+
+  render() {
+    const el = document.createElement('div');
+    el.innerHTML = this.template;
+    this.createRefs(el);
+    this.element = el.firstElementChild;
+  }
+
+  initEventListeners() {
+    [
+      this.refs.thumbL,
+      this.refs.thumbR,
+    ].forEach(thumb => thumb.addEventListener('pointerdown', event => {
+      event.preventDefault();
+
+      this.getInitialPosition(event);
+
+      const el = event.target.closest('[data-element]');
+      this.currentThumb = el && el.dataset.element;
+
+      document.addEventListener('pointermove', this.onPointerMove);
+      document.addEventListener('pointerup', this.onPointerUp);
+    }));
+  }
+
+  removeEventListeners() {
+    document.removeEventListener('pointerup', this.onPointerUp);
+    document.removeEventListener('pointermove', this.onPointerMove);
+  }
+
+  getInitialPosition(event) {
+    const { clientX } = event;
+    const sliderRect = this.refs.slider.getBoundingClientRect();
+    const thumbLRect = this.refs.thumbL.getBoundingClientRect();
+    const thumbRRect = this.refs.thumbR.getBoundingClientRect();
+
+    this.position.thumbL.shiftX = clientX - thumbLRect.left;
+    this.position.thumbR.shiftX = thumbRRect.right - clientX;
+    this.position.thumbL.sliderLeft = sliderRect.left;
+    this.position.thumbR.sliderRight = sliderRect.right;
+  }
+
+  onPointerUp = event => {
+    const customEvent = new CustomEvent('range-select', {
+      bubbles: true,
+      detail: { ...this.selection },
+    });
+
+    this.element.dispatchEvent(customEvent);
+    this.removeEventListeners();
+  }
+
+  restrictNumberByLimits(n, min, max) {
+    if (n < min) {
+      return min;
+    }
+    if (n > max) {
+      return max;
+    }
+    return n;
+  }
+
+  onPointerMove = event => {
+    const { clientX } = event;
+    const { thumbL, thumbR } = this.position;
+    const sliderWidth = this.refs.slider.getBoundingClientRect().width;
+    const rightEdge = sliderWidth - thumbL.last;
+    const leftEdge = sliderWidth - thumbR.last;
+
+    let newRight = (thumbR.shiftX + thumbR.sliderRight) - clientX;
+    let newLeft = clientX - (thumbL.shiftX + thumbL.sliderLeft);
+
+    newRight = this.restrictNumberByLimits(newRight, 0, rightEdge);
+    newLeft = this.restrictNumberByLimits(newLeft, 0, leftEdge);
+
+    switch (this.currentThumb) {
+      case 'thumbR':
+        this.moveRightThumb(newRight, sliderWidth);
+        break;
+      case 'thumbL':
+        this.moveLeftThumb(newLeft, sliderWidth);
+        break;
+      // no default
+    }
+  }
+
+  moveRightThumb(px, width) {
+    this.position.thumbR.last = px;
+    this.refs.thumbR.style.right = `${px}px`;
+    this.refs.progress.style.right = `${px}px`;
+    this.updateTo(px, width);
+  }
+
+  moveLeftThumb(px, width) {
+    this.position.thumbL.last = px;
+    this.refs.thumbL.style.left = `${px}px`;
+    this.refs.progress.style.left = `${px}px`;
+    this.updateFrom(px, width);
+  }
+
+  updateTo(px, width) {
+    const to = Math.floor(
+      this.max - this.range * this.getDecimalPercent(px, width)
+    );
+    this.selection.to = to;
+    this.refs.to.innerHTML = `${this.formatValue(to)}`;
+  }
+
+  updateFrom(px, width) {
+    const from = Math.floor(
+      this.min + this.range * this.getDecimalPercent(px, width)
+    );
+    this.selection.from = from;
+    this.refs.from.innerHTML = `${this.formatValue(from)}`;
+  }
+
+  getDecimalPercent(n, total) {
+    return (n / total * 100) / 100;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.removeEventListeners();
+    this.remove();
+  }
 }
